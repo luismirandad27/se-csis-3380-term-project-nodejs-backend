b4b253dbf9de522c814a31e7c6c94c9eaebb33cd
Andrea Olivares <alejandraolivares06@gmail.com>
2024-03-13 22:23:15 -0700

    Adding pagination and sorting

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -156,171 +156,227 @@ exports.getUniqueCountries = (req, res) => {
 };
 
 
+const getPaginationInfo = (query) => {
+    const pageSize = 9;
+    const page = parseInt(query.page) || 1;
+    return { pageSize, page };
+};
+
+const constructFilters = async (query) => {
+    let generalCondition = {}; 
+    let subtypeCondition = {};
+
+    // Category Filter
+    if (query.category) {
+        const categoryNames = query.category.split(',');
+        const categories = await ProductCategory.find({ name: { $in: categoryNames } }, '_id');
+        if (categories.length) {
+            generalCondition.product_category = { $in: categories.map(cat => cat._id) };
+        }
+    }
+    
+    // Grind Filter
+    if (query.grind) {
+        const grindNames = query.grind.split(',');
+        const grinds = await GrindType.find({ name: { $in: grindNames } }, '_id');
+        if (grinds.length) {
+            generalCondition.grind_types = { $in: grinds.map(grind => grind._id) };
+        }
+    }
+
+    // Country Filter
+    if (query.country) {
+        generalCondition.countries_origin = query.country;
+    }
+
+    // Weight Filter
+    if (query.weight) {
+        const weightValue = query.weight;
+        const weights = await WeightType.find({ name: weightValue }, '_id');
+        if (weights.length) {
+            subtypeCondition["product_subtypes.weight._id"] = { $in: weights.map(weight => weight._id) };
+        }
+    }
+
+    // Price Filter
+    if (query.min) {
+        subtypeCondition["product_subtypes.price"] = { $gte: parseFloat(query.min) }; 
+    }
+
+    if (query.max) {
+        subtypeCondition["product_subtypes.price"] = { ...subtypeCondition["product_subtypes.price"], $lte: parseFloat(query.max) }; // Merge with existing conditions if any
+    }
+
+     // Sorting
+     let sortCondition = {};
+     if (!query.sort) {
+         sortCondition = { "_id": 1 }; // Default sorting
+     } else if (query.sort === "price-low-to-high") {
+         sortCondition = { "product_subtypes.price": 1 }; //Ascending
+     } else if (query.sort === "price-high-to-low") {
+         sortCondition = { "product_subtypes.price": -1 }; //Descending
+     } else if (query.sort === "rating") {
+         sortCondition = { "average_rating": -1 };
+     } else {
+         sortCondition = { "_id": 1 }; 
+     }
+
+    return { generalCondition, subtypeCondition, sortCondition};
+};
+
+
+const getPipeline = (generalCondition, subtypeCondition, sortCondition) => {
+    let pipeline = [
+        { //We filter based on the main fields (product_category, grind_types, countries_origin)
+            $match: generalCondition
+        },
+        {//We shape our data to show only result from product_subtypes that have stock > 0
+            $project: {
+                product_subtypes: {
+                    $filter: {
+                        input: "$product_subtypes",
+                        as: "subtype",
+                        cond: { $gt: ["$$subtype.stock", 0] } 
+                    }
+                },
+                // Include all other fields, if you dont want to include a field, set it to 0
+                name: 1,
+                prod_id: 1,
+                description: 1,
+                product_category: 1,
+                countries_origin: 1,
+                tags: 1,
+                reviews: 1,
+                farm: 1,
+                producer: 1,
+                region: 1,
+                process: 1,
+                import_partner: 1,
+                grind_types: 1,
+            }
+        },
+        {//We filter the documents based on whether the product_subtypes array result from previous steps is not empty
+            $match: {
+                "product_subtypes.0": { $exists: true } 
+              }
+          },           
+        {// We populate necessary references
+            $lookup: {
+                from: "productcategories",
+                localField: "product_category",
+                foreignField: "_id",
+                as: "product_category"
+            }
+        },
+        {
+            $lookup: {
+                from: "grindtypes",
+                localField: "grind_types",
+                foreignField: "_id",
+                as: "grind_types"
+            }
+        },
+        {
+            $lookup: {
+                from: "importpartners",
+                localField: "import_partner",
+                foreignField: "_id",
+                as: "import_partner"
+            }
+        },
+        //Unwind necessary arrays, resulted from the lookups. It creates a new document for each element in the array.
+        { $unwind: "$product_subtypes" },          
+        { //We populate the weight field in product_subtypes with the corresponding data from the weighttypes collection, this is replaced by an array
+            $lookup: {
+                from: "weighttypes",
+                localField: "product_subtypes.weight",
+                foreignField: "_id",
+                as: "product_subtypes.weight"
+            }
+        },
+        { //Since we do not want arrays, we replace the array with the first element of the array, because there is only one matching element
+            $addFields: {
+                "product_category": {
+                    $arrayElemAt: ["$product_category", 0]
+                },
+                "import_partner": {
+                    $arrayElemAt: ["$import_partner", 0]
+                },
+                "product_subtypes.weight": {
+                    $arrayElemAt: ["$product_subtypes.weight", 0] 
+                },
+                "average_rating": {
+                    $cond: {
+                        if: { $isArray: "$reviews" },
+                        then: {
+                            $avg: "$reviews.rating" 
+                        },
+                        else: 0
+                    }
+                },
+            }
+        },
+        {// Given that we have unwound the product_subtypes array, we filter each document based on the conditions for product_subtypes
+            $match: subtypeCondition               
+        },
+        {// We group the documents back together, using the _id as the grouping key
+            $group: {
+                _id: "$_id", // Use the original document's _id as the grouping key
+                name: { $first: "$name" }, // Take the first encountered name value
+                prod_id: { $first: "$prod_id" }, 
+                description: { $first: "$description" }, 
+                product_category: { $first: "$product_category" }, 
+                countries_origin: { $first: "$countries_origin" }, 
+                tags: { $first: "$tags" }, 
+                average_rating: { $first: "$average_rating" },
+                reviews: { $first: "$reviews" }, 
+                farm: { $first: "$farm" },
+                producer: { $first: "$producer" },
+                region: { $first: "$region" },
+                process: { $first: "$process" },
+                import_partner: { $first: "$import_partner" }, 
+                grind_types: { $first: "$grind_types" }, 
+                product_subtypes: { $push: "$product_subtypes" }, // Reconstruct the product_subtypes array
+            }
+        },  
+        {//Sorting according to query.sort
+            $sort: sortCondition
+        }
+    ];   
+    return pipeline;
+};
+
+
+
 //Get all producst with filters & stock > 0
 exports.getAllProducts = async (req, res) => {
     try {
         const query = req.query;
+        const { pageSize, page } = getPaginationInfo(req.query); // Pagination
+        const { generalCondition, subtypeCondition, sortCondition } = await constructFilters(query); // Filters
+        const basePipeline = getPipeline(generalCondition, subtypeCondition, sortCondition); // Pipeline
 
-        // Category Filter
-        let categoryFilter = {};
-        if (query.category) {
-            const categoryNames = query.category.split(',');
-            const categories = await ProductCategory.find({ name: { $in: categoryNames } }, '_id');
-            categoryFilter = categories.length ? { $in: categories.map(cat => cat._id) } : {};
-        }
-
-        // Grind Filter
-        let grindFilter = {};
-        if (query.grind) {
-            const grindNames = query.grind.split(',');
-            const grinds = await GrindType.find({ name: { $in: grindNames } }, '_id');
-            grindFilter = grinds.length ? { $in: grinds.map(grind => grind._id) } : {};
-        }
-
-        // Weight Filter
-        let weightFilter = {};
-        if (query.weight) {
-            const weightValue = query.weight;
-            const weights = await WeightType.find({ name: weightValue }, '_id');
-            weightFilter = weights.length ? { $in: weights.map(weight => weight._id) } : {};
-        }
-
-        // Conditions for filtering the main fields
-        let generalCondition = {}; 
-        if (query.category) {
-            generalCondition.product_category = categoryFilter;
-        }
-        if (query.grind) {
-            generalCondition.grind_types = grindFilter;
-        }
-        if (query.country) {
-            generalCondition.countries_origin = query.country;
-        }
-
-        // Conditions for filtering product_subtypes fields
-        let subtypeCondition = {}; 
-        if (query.min) {
-            subtypeCondition["product_subtypes.price"] = { $gte: parseFloat(query.min) }; 
-        }
-
-        if (query.max) {
-            subtypeCondition["product_subtypes.price"] = { ...subtypeCondition["product_subtypes.price"], $lte: parseFloat(query.max) }; // Merge with existing conditions if any
-        }
-
-        if (query.weight) {
-            subtypeCondition["product_subtypes.weight._id"] = weightFilter;
-        }
-
-        // We define the pipeline
-        let pipeline = [
-            { //We filter based on the main fields (product_category, grind_types, countries_origin)
-                $match: generalCondition
-            },
-            {//We shape our data to show only result from product_subtypes that have stock > 0
-                $project: {
-                    product_subtypes: {
-                        $filter: {
-                            input: "$product_subtypes",
-                            as: "subtype",
-                            cond: { $gt: ["$$subtype.stock", 0] } 
-                        }
-                    },
-                    // Include all other fields, if you dont want to include a field, set it to 0
-                    name: 1,
-                    prod_id: 1,
-                    description: 1,
-                    product_category: 1,
-                    countries_origin: 1,
-                    tags: 1,
-                    reviews: 1,
-                    farm: 1,
-                    producer: 1,
-                    region: 1,
-                    process: 1,
-                    import_partner: 1,
-                    grind_types: 1,
-                }
-            },
-            {//We filter the documents based on whether the product_subtypes array result from previous steps is not empty
-                $match: {
-                    "product_subtypes.0": { $exists: true } 
-                  }
-              },           
-            {// We populate necessary references
-                $lookup: {
-                    from: "productcategories",
-                    localField: "product_category",
-                    foreignField: "_id",
-                    as: "product_category"
-                }
-            },
-            {
-                $lookup: {
-                    from: "grindtypes",
-                    localField: "grind_types",
-                    foreignField: "_id",
-                    as: "grind_types"
-                }
-            },
-            {
-                $lookup: {
-                    from: "importpartners",
-                    localField: "import_partner",
-                    foreignField: "_id",
-                    as: "import_partner"
-                }
-            },
-            //Unwind necessary arrays, resulted from the lookups. It creates a new document for each element in the array.
-            { $unwind: "$product_category" },
-            { $unwind: "$import_partner" },
-            { $unwind: "$product_subtypes" },          
-            { //We populate the weight field in product_subtypes with the corresponding data from the weighttypes collection, this is replaced by an array
-                $lookup: {
-                    from: "weighttypes",
-                    localField: "product_subtypes.weight",
-                    foreignField: "_id",
-                    as: "product_subtypes.weight"
-                }
-            },
-            { //Since we do not want an array, we replace the array with the first element of the array, because there is only one matching element
-                $addFields: {
-                    "product_subtypes.weight": {
-                        $arrayElemAt: ["$product_subtypes.weight", 0] 
-                    }
-                }
-            },
-            {// Given that we have unwound the product_subtypes array, we filter each document based on the conditions for product_subtypes
-                $match: subtypeCondition            
-                
-            },
-            {// We group the documents back together, using the _id as the grouping key
-                $group: {
-                    _id: "$_id", // Use the original document's _id as the grouping key
-                    name: { $first: "$name" }, // Take the first encountered name value
-                    prod_id: { $first: "$prod_id" }, 
-                    description: { $first: "$description" }, 
-                    product_category: { $first: "$product_category" }, 
-                    countries_origin: { $first: "$countries_origin" }, 
-                    tags: { $first: "$tags" }, 
-                    reviews: { $first: "$reviews" }, 
-                    farm: { $first: "$farm" },
-                    producer: { $first: "$producer" },
-                    region: { $first: "$region" },
-                    process: { $first: "$process" },
-                    import_partner: { $first: "$import_partner" }, 
-                    grind_types: { $first: "$grind_types" }, 
-                    product_subtypes: { $push: "$product_subtypes" }, // Reconstruct the product_subtypes array
-                   
-                }
-            }
-        ];
-
-        // Execute the pipeline
-        const products = await Product.aggregate(pipeline);
-        products.sort((a, b) => a._id.toString().localeCompare(b._id.toString()));
-        res.send(products);
+        // Count pipeline: we count the documents that match the filters
+        const countPipeline = [...basePipeline, { $count: "totalCount" }];  
+        const totalDocuments = await Product.aggregate(countPipeline);
+        const totalCount = totalDocuments.length > 0 ? totalDocuments[0].totalCount : 0;
+        
+        // Main pipeline: we get the documents that match the filters and apply pagination
+        const mainPipeline = [ ...basePipeline, { $skip: (page - 1) * pageSize }, { $limit: pageSize } ];
+        const paginatedProducts = await Product.aggregate(mainPipeline);
+ 
+        //console.log(paginatedProducts);
+        res.json({
+            totalPages: Math.ceil(totalCount / pageSize),
+            page,
+            products: paginatedProducts,
+            totalCount
+        });
     } catch (err) {
         console.error("Error getting products:", err);
         res.status(500).send({ message: err });
     }
 };
+
+
+
