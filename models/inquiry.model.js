3c0ff92574fb8748068db738cd0cc480c8927b3c
Andrea Olivares <alejandraolivares06@gmail.com>
2024-03-30 01:22:50 -0700

    Fixing field for signup, adding inquiry field

--- models/inquiry.model.js
+++ models/inquiry.model.js
@@ -5,7 +5,11 @@ const InquirySchema = new mongoose.Schema({
     body: String,
     inquiry_date: Date,
     email: String,
-    phone: String
+    phone: String,
+    opened: {
+        type: Date,
+        default: null
+    }
 });
 
 const Inquiry = mongoose.model("Inquiry", InquirySchema);
